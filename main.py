961fc3d3b655bd8a1da9d0adb1d3474fbab4bcd2
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-17 18:21:01 -0800

    simplifying code

--- main.py
+++ main.py
@@ -5,54 +5,59 @@ class Task:
     def __init__(self,name,deadline):
         self.name = name
         self.deadline = deadline
+        self.daysRemaining = ""
     
-    def update_daysLeft(self,days_until):
-        self.daysLeft = days_until
+    # def daysRemaining(self,days_remaining):
+    #     self.days_remaining = days_remaining
     
     def __str__(self):
-        return f'name: {self.name}, deadline: {self.deadline}, daysleft: {self.daysLeft}'
+        return f'name: {self.name}, deadline: {self.deadline}, remaining days: {self.daysRemaining}'
 
 class Task_Encoder(json.JSONEncoder):
     
     def default(self,obj):
-        d = {"name":obj.name, "deadline":obj.deadline, "daysLeft": obj.daysLeft}
+        d = {"name":obj.name, "deadline":obj.deadline, "Remaing days": obj.daysRemaining}
         return d
         
              
         
 class Manager:
-    def __init__(self,data):
-        self.data = data
-        self.Task_list = self.data["Tasks"]
-        
-        
+    def __init__(self,jsonStream):
+        self.data = jsonStream
+        self.Tasks = self.data["Tasks"]
+             
     def set_data(self,data):
         self.data = data
     
     def add_task(self):
         name = input("Task: ")
-        deadline = input("deadline: ")
-    
+        given_deadline = input("Deadline: ")
+        
+        if given_deadline == 'today':
+            deadline = str(datetime.today())
+        else:
+            deadline = given_deadline
+        
         new_task = Task(name,deadline)
-        self.calc_days_left(new_task)
-        self.Task_list.append(new_task)
+        self.calculate_RemainingDays(new_task)
+        self.Tasks.append(new_task)
     
-    def calc_days_left(self, t):
+    def calculate_RemainingDays(self, task):
         now = datetime.today()
-        days_left = datetime.strptime(t.deadline, "%Y-%m-%d") - now
-        t.update_daysLeft(str(days_left))
+        days_left = datetime.strptime(task.deadline, "%Y-%m-%d") - now
+        task.daysRemaining = str(days_left)
     
     def decision(self,choice):
         options = ["add task","disp all","q"]
         
         if choice not in options:
             print("unsupported action")
-            
-        
+
         if choice == options[0]:
             self.add_task()
+            
         elif choice == options[1]:
-            for t in self.Task_list:
+            for t in self.Tasks:
                 print(t)   
         else:
             print("END")
@@ -62,6 +67,7 @@ class Manager:
 if __name__ == '__main__':
     
     # Loads data from usr_data file
+    
     with open('usr_data.json') as jFile:
         data = json.load(jFile)
     
@@ -73,6 +79,7 @@ if __name__ == '__main__':
         J_Man.decision(choice)
     
     # Update Tasks.daysLeft
+    
     # for t in J_Man.Task_list:
     #     J_Man.calc_days_left(t)
     
@@ -81,15 +88,7 @@ if __name__ == '__main__':
         json.dump(J_Man.data,jFile,indent=4,cls=Task_Encoder)
 
         
-
-        
-        
-    
-    
-    
-        
-        
-        
+      
         
     
     
