ce643cf09f3d98eb1936a3350b2d1778a436c935
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-26 20:58:13 -0800

    init TaskFile

---
+++ Task.py
@@ -0,0 +1,13 @@
+# Task Definition
+
+
+
+# TaskManager 
+    # properties are pointing to the appropriate places in memory
+    
+        # time_attr = Task['Time']['RemainingTime'], Could be used to update the remaining time.
+        # many more needed.
+    
+    # load json into a Task List
+    
+    # save json inot a json file
