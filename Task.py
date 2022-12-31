1d6e5b65b5dd2559e3e811378f87db60ee0a398b
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-30 20:23:31 -0800

    Task Base implemented

--- Task.py
+++ Task.py
@@ -4,25 +4,13 @@ class Task:
         self.name = name
         self.deadline = datetime.strptime(deadline,"%Y-%m-%d")
         self.remainingTime = self.deadline - datetime.today()
-        
-        
     
-    ##  METHODs
-    
-        # update method is_completed attribute 
-            # record the time of completion
-        
-        # 
+    def __str__(self):
+        return f'{self.name}, {self.deadline}, {self.remainingTime}' 
 
-
-
-
-# TaskManager 
+# TaskManager
     # properties are pointing to the appropriate places in memory
     
         # time_attr = Task['Time']['RemainingTime'], Could be used to update the remaining time.
         # many more needed.
     
-    # load json into a Task List
-    
-    # save json inot a json file
