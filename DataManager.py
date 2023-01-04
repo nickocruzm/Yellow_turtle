ecdad5ef859698c3a716ce74b9e61547ea0fb535
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-03 21:37:57 -0800

    init file

--- DataManager.py
+++ DataManager.py
@@ -1,6 +1,33 @@
 import sys
+from datetime import datetime
 import pandas as pd
+from Task import Task
 
-for l in sys.path:
-    print(l)
+
+
+class Manager:
+    def __init__(self,fileName):
+        self.fileName = fileName
+    
+    def read(self):
+        self.data = pd.read_json(self.fileName)
+        type(self.data)
+        
+        
+    def data_to_Task(self) -> Task:
+        for t in self.data:
+            name = t
+            d = self.data[t]['deadline']
+            j_Tags = list(self.data[t]['tags'])
+            new_data = Task(name, d, tags=j_Tags)
+            
+            yield new_data
+            
+
+
+
+
+    
+
+
 
