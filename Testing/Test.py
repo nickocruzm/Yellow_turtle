30eb0351bc49e8103952dfa28d4e5b9162778047
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-03 21:36:58 -0800

    Simple Tests

---
+++ Testing/Test.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('..')
+from Task import Task, ToDoList
+from DataManager import Manager
+
+# dumby data
+t1 = Task("task_1", "2023-10-11")
+t2 = Task("task_2", "2023-11-11")
+t3 = Task("task_3", "2022-09-11")
+
+tasks = [t1,t2,t3]
+todoList = ToDoList(tasks)
+
+def test_display():
+    todoList.display_all()
+
+def test_get():
+    taskName = "task_1"
+    todoList.get_task(taskName)
+
+def test_updateTags():
+    todoList.update_tags("task_3", "TestData")
+    
+
+def test_manager():
+    man = Manager("../test.json")
+    man.read()
+    task_list = list( man.data_to_Task() )
+    
+    for t in task_list:
+        print(t)
+
+
+
+
+
+
+
+
+
