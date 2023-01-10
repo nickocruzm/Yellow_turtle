6e00e96c502b3025e5674da8e58bd058e3432ce5
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-09 16:16:35 -0800

    init file

---
+++ Timeline/Timeline.py
@@ -0,0 +1,5 @@
+
+# read in objects associated with a certain project or Goal
+# sort the objects in appropriate order, based on the deadlines.
+# write the objects into a file, so that we can load in the TimeLine directly next time
+# updating the file directly if necessary.
