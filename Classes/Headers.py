9eaeb319c4c7fb988bbfdef25ce213e28ba7dae1
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-10 17:01:57 -0800

    removed stupid file

--- Classes/Headers.py
+++
@@ -1,8 +0,0 @@
-import json
-#import pandas as pd
-from Program import Program
-from Converters import JSON_Translator
-from datetime import date, datetime
-from Task import Task, ToDoList
-from usr_choices import *
-
